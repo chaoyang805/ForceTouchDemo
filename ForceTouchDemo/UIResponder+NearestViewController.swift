27793c51dd9cdfc28b63095a914ee35e00d055ba
chaoyang805 <zhangchaoyang805@gmail.com>
2016-10-31 23:49:52 +0800

    add share and copy feature

---
+++ ForceTouchDemo/UIResponder+NearestViewController.swift
@@ -0,0 +1,9 @@
+//
+//  UIResponder+NearestViewController.swift
+//  ForceTouchDemo
+//
+//  Created by chaoyang805 on 2016/10/31.
+//  Copyright © 2016年 chaoyang805. All rights reserved.
+//
+
+import Foundation
